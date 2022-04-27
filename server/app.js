a38ae0067b7768e5ebfd6dcb414f2c86be0fb513
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-04-27 00:26:55 -0500

    Serve static content for production or hot reload for development

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const cors = require('cors');
 const express = require('express');
+const path = require('path');
+const chokidar = require('chokidar');
 require('express-async-errors');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
@@ -38,6 +40,52 @@ app.use('/api/blogs', blogsRouter);
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing');
   app.use('/api/testing', testingRouter);
+}
+
+/**
+ *  Use "hot loading" in backend
+ */
+const watcher = chokidar.watch('server'); // Watch server folder
+watcher.on('ready', () => {
+  watcher.on('all', () => {
+    Object.keys(require.cache).forEach((id) => {
+      if (id.includes('server')) delete require.cache[id]; // Delete all require caches that point to server folder (*)
+    });
+  });
+});
+
+logger.info(`Running in production: ${config.IN_PRODUCTION}`);
+
+/**
+ * For frontend use hot loading when in development, else serve the static content
+ */
+if (!config.IN_PRODUCTION) {
+  const webpack = require('webpack');
+  const middleware = require('webpack-dev-middleware');
+  const hotMiddleWare = require('webpack-hot-middleware');
+  const webpackConf = require('../webpack.config');
+
+  const compiler = webpack(webpackConf('development', { mode: 'development' }));
+
+  const devMiddleware = middleware(compiler);
+  app.use(devMiddleware);
+  app.use(hotMiddleWare(compiler));
+  app.use('*', (req, res, next) => {
+    const filename = path.join(compiler.outputPath, 'index.html');
+    devMiddleware.waitUntilValid(() => {
+      compiler.outputFileSystem.readFile(filename, (err, result) => {
+        if (err) return next(err);
+        res.set('content-type', 'text/html');
+        res.send(result);
+        return res.end();
+      });
+    });
+  });
+} else {
+  const DIST_PATH = path.resolve(__dirname, '../dist');
+  const INDEX_PATH = path.resolve(DIST_PATH, 'index.html');
+  app.use(express.static(DIST_PATH));
+  app.get('*', (req, res) => res.sendFile(INDEX_PATH));
 }
 
 app.use(middleware.unknownEndpoint);
